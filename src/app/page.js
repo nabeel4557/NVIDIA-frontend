ad92df63332c9f9e1a079d5cb970f88e5e4a4927
nabeel4557 <muhammednabeel57@gmail.com>
2024-10-14 00:54:04 +0530

    Update

--- src/app/page.js
+++ src/app/page.js
@@ -1,12 +1,16 @@
 import Image from "next/image";
 import HomePage from "./components/homepage/page";
 import Industries from "./industries/page";
+import Solutions from "./solution/page";
+
 
 export default function Home() {
   return (
     <>
       <HomePage />
-      <Industries/>
+      <Industries />
+      <Solutions/>
+      
     </>
     
     
---
+++ src/app/solution/page.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+import Ai from './ai/page'
+
+function Solutions() {
+  return (
+      <div>
+          <Ai/>
+          
+    </div>
+  )
+}
+
+export default Solutions;
