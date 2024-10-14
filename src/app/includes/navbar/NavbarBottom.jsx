d2b37078283c9e3a514b4d83cd4c125fefe9205a
nabeel4557 <muhammednabeel57@gmail.com>
2024-10-14 14:18:43 +0530

    Update

--- src/app/includes/navbar/NavbarBottom.jsx
+++ src/app/includes/navbar/NavbarBottom.jsx
@@ -1,34 +1,147 @@
+"use client";
+
 import Link from "next/link";
-import React from "react";
-import { IoMdArrowDropdown } from "react-icons/io";
+import React, { useState } from "react";
+import { IoMdArrowDropdown, IoMdArrowDropup } from "react-icons/io";
 
 function NavbarBottom() {
+    const [isSolutionsDropdownOpen, setIsSolutionsDropdownOpen] = useState(false);
+    const [isSoftwareDropdownOpen, setIsSoftwareDropdownOpen] = useState(false);
+
+    const toggleSolutionsDropdown = () => {
+        setIsSolutionsDropdownOpen(!isSolutionsDropdownOpen);
+    };
+
+    const toggleSoftwareDropdown = () => {
+        setIsSoftwareDropdownOpen(!isSoftwareDropdownOpen);
+    };
+
     return (
-        <div className="bg-[#000] sticky top-0 z-20 ">
-            <div className="wrapper flex items-center py-4 gap-[25px] ">
+        <div className="bg-[#000] sticky top-0 z-20">
+            <div className="wrapper flex items-center py-4 gap-[25px]">
                 <div className="">
-                    <h2 className="text-[28px] ">Artificial Intelligence</h2>
+                    <h2 className="text-[28px] text-white">
+                        Artificial Intelligence
+                    </h2>
                 </div>
-                <div className="flex items-center ">
+                <div className="flex items-center">
                     <ul className="flex items-center text-[16px] gap-[25px] mt-2 text-[#999999]">
                         <li>
                             <Link href="/industries">Industries</Link>
                         </li>
-                        <li className="flex items-center gap-2">
+                        <li
+                            className="flex items-center gap-2 relative"
+                            onClick={toggleSolutionsDropdown}>
                             <Link href="#">Solutions</Link>
-                            <IoMdArrowDropdown size={20} className=" mt-1" />
+                            {isSolutionsDropdownOpen ? (
+                                <IoMdArrowDropup
+                                    size={20}
+                                    className="mt-1 cursor-pointer"
+                                />
+                            ) : (
+                                <IoMdArrowDropdown
+                                    size={20}
+                                    className="mt-1 cursor-pointer"
+                                />
+                            )}
+                            {isSolutionsDropdownOpen && (
+                                <ul className="absolute w-[220px] top-10 bg-[#fff] text-[16px] text-[#1A1A1A] px-[40px] py-[25px] leading-10 z-10">
+                                    <li>
+                                        <Link href="#">Overview</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="/solution/ai">
+                                            AI Platform
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">AI Inference</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">AI Workflows</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">Conversational AI</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">Data Analytics</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">Generative AI</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">Machine Learning</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">
+                                            Prediction and Forecasting
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">Speech AI</Link>
+                                    </li>
+                                </ul>
+                            )}
                         </li>
-                        <li className="flex items-center gap-2">
+                        <li
+                            className="flex items-center gap-2 relative"
+                            onClick={toggleSoftwareDropdown}>
                             <Link href="#">Software</Link>
-                            <IoMdArrowDropdown size={20} className=" mt-1" />
+                            {isSoftwareDropdownOpen ? (
+                                <IoMdArrowDropup
+                                    size={20}
+                                    className="mt-1 cursor-pointer"
+                                />
+                            ) : (
+                                <IoMdArrowDropdown
+                                    size={20}
+                                    className="mt-1 cursor-pointer"
+                                />
+                            )}
+                            {isSoftwareDropdownOpen && (
+                                <ul className="absolute w-[220px] top-10 bg-[#fff] text-[16px] text-[#1A1A1A] px-[40px] py-[25px] leading-10 z-10">
+                                    <li>
+                                        <Link href="#">
+                                            AI Enterprise Suite
+                                        </Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">AI Inference - Triton</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">AI Workflows</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">Cybersecurity - Morpheus</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">Conversational AI</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">Data Analytics</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">Generative AI</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">Machine Learning</Link>
+                                    </li>
+                                    <li>
+                                        <Link href="#">
+                                            Prediction and Forecasting
+                                        </Link>
+                                    </li>
+                                    
+                                </ul>
+                            )}
                         </li>
                         <li className="flex items-center gap-2">
                             <Link href="#">Products</Link>
-                            <IoMdArrowDropdown size={20} className=" mt-1" />
+                            <IoMdArrowDropdown size={20} className="mt-1" />
                         </li>
                         <li className="flex items-center gap-2">
                             <Link href="#">Resources</Link>
-                            <IoMdArrowDropdown size={20} className=" mt-1" />
+                            <IoMdArrowDropdown size={20} className="mt-1" />
                         </li>
                     </ul>
                 </div>
