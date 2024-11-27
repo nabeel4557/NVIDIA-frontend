bbc9b941b31e525ceb16b322128711bcaa215c94
nabeel4557 <muhammednabeel57@gmail.com>
2024-11-27 11:20:30 +0530

    Update

--- src/components/case/page.jsx
+++ src/components/case/page.jsx
@@ -4,7 +4,7 @@ import { MdKeyboardArrowRight } from "react-icons/md";
 function Case() {
     return (
         <div className="w-full bg-[#EEEEEE] text-[#000] py-[75px] max-[540px]:w-full">
-            <div className="wrapper max-[1280px]:w-[80%] max-[1080px]:w-[90%] max-[786px]:w-[80%] max-[640px]:w-[90%] max-[540px]:w-full max-[540px]:px-[10px] ">
+            <div className="wrapper max-[1280px]:w-[80%] max-[1080px]:w-[90%] max-[786px]:w-full max-[640px]:w-[90%] max-[540px]:w-full max-[540px]:px-[10px] ">
                 <h5 className=" text-center text-[#1A1A1A] text-[20px] font-bold max-[1280px]:text-[18px]">
                     Use Cases
                 </h5>
@@ -16,8 +16,8 @@ function Case() {
                     jump-start your AI development with curated examples.
                 </p>
 
-                <div className="flex  items-center mt-[40px] gap-5 max-[1080px]:w-full max-[1080px]:mx-auto max-[980px]:flex-wrap max-[980px]:w-full max-[980px]:justify-center max-[786px]:w-full max-[640px]:w-[90%] max-[540px]:w-full">
-                    <div className="w-1/3 bg-[#fff] h-[530px] relative pb-[30px] shadow-lg max-[1080px]:w-[32%] max-[980px]:w-[35%] max-[786px]:w-[280px] max-[640px]:w-[500px] max-[540px]:w-full">
+                <div className="flex  items-center mt-[40px] gap-7 max-[1080px]:w-full max-[1080px]:mx-auto max-[980px]:flex-wrap max-[980px]:w-full max-[980px]:justify-center max-[786px]:w-full max-[640px]:w-[90%] max-[540px]:w-full">
+                    <div className="w-1/3 bg-[#fff] h-[530px] max-[980px]:h-[500px] max-[640px]:h-[550px] relative pb-[30px] shadow-lg max-[1080px]:w-[32%] max-[980px]:w-[35%] max-[786px]:w-[35%] max-[640px]:w-[500px] max-[540px]:w-full">
                         <div className="w-full max-[640px]:w-[500px] max-[540px]:w-full ">
                             <img
                                 className="max-[640px]:w-full max-[540px]:w-full"
@@ -44,7 +44,7 @@ function Case() {
                             </h6>
                         </div>
                     </div>
-                    <div className="w-1/3 bg-[#fff] h-[530px] relative pb-[30px] shadow-lg max-[1080px]:w-[32%]  max-[980px]:w-[35%] max-[786px]:w-[280px] max-[640px]:w-[500px] max-[540px]:w-full">
+                    <div className="w-1/3 bg-[#fff] h-[530px] max-[980px]:h-[500px] max-[640px]:h-[550px] relative pb-[30px] shadow-lg max-[1080px]:w-[32%]  max-[980px]:w-[35%] max-[786px]:w-[35%] max-[640px]:w-[500px] max-[540px]:w-full">
                         <div className="w-full max-[640px]:w-[500px] max-[540px]:w-full">
                             <img
                                 className="max-[640px]:w-full max-[540px]:w-full"
@@ -70,7 +70,7 @@ function Case() {
                             </h6>
                         </div>
                     </div>
-                    <div className="w-1/3 bg-[#fff] h-[530px] relative pb-[30px] shadow-lg max-[1080px]:w-[32%]  max-[980px]:w-[35%]  max-[786px]:w-[47%]  max-[640px]:w-[100%] max-[540px]:w-full">
+                    <div className="w-1/3 bg-[#fff] h-[530px] max-[980px]:h-[500px] max-[640px]:h-[550px] relative pb-[30px] shadow-lg max-[1080px]:w-[32%]  max-[980px]:w-[35%] max-[786px]:w-[35%] max-[640px]:w-[500px] max-[540px]:w-full">
                         <div className="w-full max-[640px]:w-[500px] max-[540px]:w-full">
                             <img
                                 className="max-[640px]:w-full max-[540px]:w-full"
